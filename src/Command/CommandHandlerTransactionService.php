a7debf81418673d4ea3bfe5d7071ff135a52ab2c
Franck DAKIA <dakiafranck@gmail.com>
2023-08-20 11:45:25 +0000

    Add the command transaction manager service

---
+++ src/Command/CommandHandlerTransactionService.php
@@ -0,0 +1,34 @@
+<?php
+
+use Bow\CQRS\Command\CommandInterface;
+use Bow\CQRS\Command\CommandHandlerInterface;
+use Bow\Database\Database;
+use Bow\Database\Exception\DatabaseException;
+
+abstract class CommandHandlerTransactionService implements CommandHandlerInterface
+{
+    /**
+    * @param CommandInterface $command
+    * @return mixed
+    */
+    abstract protected function run(CommandInterface $command): mixed;
+    
+    /**
+     * @inheritDoc
+     */
+    public function process(CommandInterface $command): mixed
+    {
+        Database::startTransaction();
+
+        try {
+            $result = $this->run($command);
+
+            Database::commit();
+
+            return $result;
+        } catch (DatabaseException $e) {
+            Database::rollback();
+            throw $e;
+        }
+    }
+}
